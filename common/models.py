99887c07350a4d1530c1d6d8f341b72ada5a004e
donghapark <donghaha12@naver.com>
2023-05-09 21:32:33 +0900

    진짜로그인

--- common/models.py
+++ common/models.py
@@ -1,8 +1,3 @@
-
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    nickname = models.CharField(max_length=200)
-    mbti = models.CharField(max_length=200)
 # Create your models here.
